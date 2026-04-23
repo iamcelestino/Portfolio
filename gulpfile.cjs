07eab5fd4750e61e960444d5475229f3f3d26a8b
iamcelestino <trcelestino488@gmail.com>
2026-04-23 01:20:54 -0500

    chore: add scss

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -4,7 +4,7 @@ const sass = require('gulp-sass')(require('sass'));
 function buildStyles() {
   return src('sass/**/*.scss')
     .pipe(sass())
-    .pipe(dest('styles'))
+    .pipe(dest('src/styles'))
 }
 
 function watchTask() {
